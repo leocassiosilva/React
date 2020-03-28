8f98c0b167676babb82cd98a9bd407d04cb4766e
leocassiosilva <leocassiosilva1234@gmail.com>
2020-03-28 08:35:02 -0300

    primeiro commit

---
+++ src/Leocassio.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import User from './User'
+
+
+class Leocassio extends Component {
+    render() {
+        return ( < div >
+            Eu sou um componente chamado Leocassio!
+            <
+            User name = "Leocassio" > < /User> <
+            /div>
+
+        );
+    }
+}
+
+export default Leocassio;
---
+++ src/User.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class User extends Component {
+    render() {
+        return ( <
+            div >
+            Nome: { this.props.name } <
+            /div>
+        );
+    }
+}
+
+export default User;
